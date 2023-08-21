5afea783e2c4226b750d42f72a0b9fc56c4d771d
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-21 16:33:27 +0200

    OOP task

--- src/alexQI/java/day45_oop/Coffee/CoffeeTest.java
+++ src/alexQI/java/day45_oop/Coffee/CoffeeTest.java
@@ -2,20 +2,28 @@ package alexQI.java.day45_oop.Coffee;
 
 import alexQI.java.day45_oop.Coffee.Coffee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoffeeTest {
     public static void main(String[] args) {
-        Coffee myCoffee = new Coffee();
-        System.out.println("coffee amount = " + myCoffee.getAmount());
-        myCoffee.refill();
-        System.out.println("amount after refill = " + myCoffee.getAmount());
-        myCoffee.drink(10);
-        System.out.println("amount after drink 10 = " + myCoffee.getAmount());
-//      NOT : myCoffee.type = "Turkish coffee"; INSTEAD using a method:
-        myCoffee.setType("Turkish coffee");
-        System.out.println("My coffee = " + myCoffee.getType());
-        //describe myCoffee - show all variable values
-        System.out.println(myCoffee.toString());
+        List<Coffee> list = new ArrayList<>();
+        while(true) {
+            Coffee myCoffee = new Coffee();
+            list.add(myCoffee);
+            System.out.println("coffee amount = " + myCoffee.getAmount());
+            myCoffee.refill();
+//            System.out.println("amount after refill = " + myCoffee.getAmount());
+//            myCoffee.drink(10);
+//            System.out.println("amount after drink 10 = " + myCoffee.getAmount());
+////      NOT : myCoffee.type = "Turkish coffee"; INSTEAD using a method:
+//            myCoffee.setType("Turkish coffee");
+//            System.out.println("My coffee = " + myCoffee.getType());
+//            //describe myCoffee - show all variable values
+//            System.out.println(myCoffee.toString());
 
-        //ADD ANOTHER COFFEE OBJECT, SET VALUES, CALL METHODS
+            //ADD ANOTHER COFFEE OBJECT, SET VALUES, CALL METHODS
+//        main(null);
+        }
     }
 }
